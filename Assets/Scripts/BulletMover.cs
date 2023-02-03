02d0a9862ea20829747996ab65af4b86f3284fdd
WickedCube <wickedcube@gmail.com>
2023-02-04 01:55:08 +0530

    Player!!!

---
+++ Assets/Scripts/BulletMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    private Rigidbody rb;
+    private WaitForSeconds waitForTwoSeconds = new WaitForSeconds(2f);
+    // Start is called before the first frame update
+    void Awake() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    IEnumerator Start()
+    {
+        rb.AddForce(transform.forward*50, ForceMode.Impulse);
+        yield return waitForTwoSeconds;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraRef;
+    public Animator animatorRef;
+    public List<GameObject> bullet;
+    public Transform bulletSpawn;
+    private Vector3 startPlayerPosition;
+    private Vector3 startCameraPosition;
+    float verticalInput;
+    float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPlayerPosition = transform.position;
+        startCameraPosition = cameraRef.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.rotation = Quaternion.Euler(0, -Input.mousePosition.x, 0);
+        transform.Translate(verticalInput * 0.1f * Vector3.forward + horizontalInput * 0.1f * Vector3.right);
+        cameraRef.position = startCameraPosition + transform.position - startPlayerPosition;
+        animatorRef.SetFloat("moveSpeedX", horizontalInput);
+        animatorRef.SetFloat("moveSpeedZ", verticalInput);
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(bullet[Random.Range(0, 3)], bulletSpawn.position, bulletSpawn.rotation);
+        }
+    }
+}
