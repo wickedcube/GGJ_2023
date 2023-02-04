6fee1587c5743c792f47c4c1087d430c52d57b17
Rutvij Joshi <rutvijjoshi99@gmail.com>
2023-02-04 19:57:24 +0530

    Leaderboard, Procedural UI and menu stuff

---
+++ Assets/APIScripts/UpdateUsername.php
@@ -0,0 +1,30 @@
+<?php
+$file = "leaderboard.json";
+
+$input = file_get_contents('php://input');
+$json = file_get_contents($file);
+$data = json_decode($json, true);
+
+$name = $input["username"];
+$score = $input["highscore"];
+
+$exists = false;
+foreach ($data as &$record) {
+    if ($record["username"] == $name) 
+    {
+        $exists = true;
+        break;
+    }
+}
+
+if (!$exists) {
+    $data[] = array("username" => $name, "highscore" => $score);
+    $newJson = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents($file, $newJson);
+    echo "success";
+}
+else{
+    echo "unavailable"; 
+}
+
+?>
