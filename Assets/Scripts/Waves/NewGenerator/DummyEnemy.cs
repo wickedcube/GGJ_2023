ae27fa4a641527c4e08f1560603c0e1fb9fdcea2
sidsayshmm <siddharthgup3@gmail.com>
2023-02-05 14:43:33 +0530

    wave starting

--- Assets/Scripts/Waves/NewGenerator/DummyEnemy.cs
+++ Assets/Scripts/Waves/NewGenerator/DummyEnemy.cs
@@ -8,7 +8,6 @@ public class DummyEnemy : MonoBehaviour
     public void Init(int num)
     { 
         // Debug.LogError($"Spawning enemy for number {num}");
-        var t = FindObjectOfType<EnemySpawner>();
-        t.CreateEnemyAt(transform.position, num);
+        
     }
 }
