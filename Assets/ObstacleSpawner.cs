b3694d252e25898e6b1b7f30a7948428e0d5e9e6
WickedCube <wickedcube@gmail.com>
2023-02-05 07:25:26 +0530

    Push

--- Assets/ObstacleSpawner.cs
+++ Assets/ObstacleSpawner.cs
@@ -1,18 +1,32 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> obstaclePrefabs;
+    public Transform playerRef;
+    private int currentCellX = -1;
+    private int currentCellY = -1;
+    private int chunkSize = 45;
+    private HashSet<Vector2> spawnedChunkedCoOrds = new HashSet<Vector2>();
+    int xCoOrd, zCoOrd;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        xCoOrd = (int)(playerRef.position.x + (chunkSize / 2)) / chunkSize;
+        zCoOrd = (int)(playerRef.position.z + (chunkSize / 2)) / chunkSize;
+        if (currentCellX != xCoOrd && currentCellY != zCoOrd)
+        {
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (spawnedChunkedCoOrds.Add(new Vector2(xCoOrd + i, zCoOrd + j)))
+                    {
+                        Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)], new Vector3((xCoOrd + i)* chunkSize - chunkSize / 2, 0, (zCoOrd + j) * chunkSize - chunkSize / 2), Quaternion.Euler(0, Random.Range(0, 4) * 90, 0));
+                    }
+                }
+            }
+        }
     }
 }
