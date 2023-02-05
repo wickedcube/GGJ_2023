7f36f575b8005cd15ddfa08023d3149f9d01299c
Rutvij Joshi <rutvijjoshi99@gmail.com>
2023-02-05 16:42:54 +0530

    null ref fix

--- Assets/Scripts/Enemy/EnemyPointer.cs
+++ Assets/Scripts/Enemy/EnemyPointer.cs
@@ -7,18 +7,18 @@ namespace Enemy
     {
         private void Start()
         {
-            FindObjectOfType<PointerManager>().Register(this);
+            FindObjectOfType<PointerManager>()?.Register(this);
         }
 
 
         private void OnBecameInvisible()
         {
-            FindObjectOfType<PointerManager>().OnEnemyInvisible(this);
+            FindObjectOfType<PointerManager>()?.OnEnemyInvisible(this);
         }
 
         private void OnBecameVisible()
         {
-            FindObjectOfType<PointerManager>().OnEnemyVisible(this);
+            FindObjectOfType<PointerManager>()?.OnEnemyVisible(this);
         }
 
         private void OnDisable()
